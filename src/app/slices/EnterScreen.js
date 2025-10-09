517bff23aa148bc654ea80b4f63e3507ff35b53e
Hunter Mann <126185686+huntdamann@users.noreply.github.com>
2025-10-09 18:22:03 -0500

    updating mobile Dark mode

--- src/app/slices/EnterScreen.js
+++ src/app/slices/EnterScreen.js
@@ -26,7 +26,7 @@ const EnterScreen = forwardRef( ({ onFinish }, ref) =>  {
 
 
             <CardContainer className="inter-var">
-            <CardBody className="bg-transparent flex flex-col items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-green-50/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 ">
+            <CardBody className="bg-transparent flex flex-col items-center relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] border-green-50/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 ">
                 <CardItem
                 translateZ="50"
                 className="text-xl text-center font-bold text-neutral-600 dark:text-white"
